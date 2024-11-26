40d1f4e0636bb3bc412eed1056eff4778884f045
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-26 15:12:39 +0100

    ADD app.jsx | completedTasks section, tasks counter

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {tasks} from './data/tasks'
 
-//const completedTasks = tasks.filter((task) =>  task.state === "completed");
+const completedTasks = tasks.filter((task) =>  task.state === "completed");
 const remainingTasks = tasks.filter((task) => task.state !== "completed");
 
 function App() {
@@ -9,8 +9,22 @@ function App() {
   return (
     <main>
       <section>
+        <h3>Current Tasks ({remainingTasks.length})</h3>
           <ul>
             {remainingTasks.map((task) =>
+            <li key={task.id}>
+              <h4>{task.title}</h4>
+              <div>{task.state}</div>
+              <p>Priority: {task.priority}</p>
+              <p>Est. Time: {task.estimatedTime}</p>
+            </li>)}
+          </ul>
+      </section>
+
+      <section>
+        <h3>Current Tasks ({completedTasks.length})</h3>
+          <ul>
+            {completedTasks.map((task) =>
             <li key={task.id}>
               <h4>{task.title}</h4>
               <div>{task.state}</div>
